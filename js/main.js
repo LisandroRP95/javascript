69a91321d6a5e5bf638b3dce8bcefeaf94afd946
Lisandro Rodriguez <licharp41@gmail.com>
2023-04-13 21:15:23 -0300

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Ejercicio clase 1
+    //let numeroUno = parseInt(prompt("ingresar primer número"));
+    //let numeroDos = parseInt(prompt("ingresar segundo número"));
+
+    //alert("El resultado es: "+ parseInt(numeroUno + numeroDos));
+
+// Ejercicio clase 2
+   
+    /*let numeroGrande = parseInt(prompt("Ingrese un número: "));
+
+    if(numeroGrande > 1000){
+        alert("El número corresponde a grupo seleccionado")
+    }; 
+    
+    Bien hecho*/ 
+
+    /* este ejemplo funciono pero no era lo que presentaba el ejercicio
+    
+    let textoSaludo = (prompt("Ingrese un saludo: "));
+
+        if(textoSaludo = "hola"){
+        alert("hola, mucho gusto")};
+    
+    Bien hecho */
+
+/*     let textoSaludo = (prompt("Ingrese un saludo: "));
+
+    if(textoSaludo === "hola"){
+    alert("hola, mucho gusto")}; 
+    
+    Bien hecho */
+
+    let numeroLoco = parseInt(prompt("Ingrese un número: "));
+
+    if(numeroLoco < 10){
+        alert("El número todavia no corresponde al grupo especial");
+    }else if(numeroLoco < 50){
+        alert("el numero es del grupo selecto")}
+    
+    else(numeroLoco > 50)
+    alert("el numero se paso del grupo selecto");
